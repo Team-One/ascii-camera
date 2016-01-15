9b7df2f066dc1be26e61dab52d420f4866d54506
Alyssa Saucedo <Alyssa.Saucedo@teamone-usa.com>
2016-01-15 14:30:55 -0800

    throwing in some code to see if it gives cam options

--- js/asciicam.js
+++ js/asciicam.js
@@ -1,4 +1,3 @@
-/*jslint browser: true */
 (function (app) {
 	"use strict";
 	var cam, intervalId, canvas, canvasCtx, ascii, btnStart, btnStop,
@@ -23,69 +22,61 @@
         btnStop.addEventListener('click', app.stopCam);
     };
 
-  //   app.startCam = function (e) {
-		// // Get specific vendor methods
-		// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-		// window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
+    app.startCam = function (e) {
+		// Get specific vendor methods
+		navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+		window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
-		// // If browser supports user media
-		// if (navigator.getUserMedia) {
-		// 	navigator.getUserMedia({video: true, toString: function () { return "video"; } },
-		// 		function successCallback(stream) {
-		// 			//if (navigator.getUserMedia === navigator.mozGetUserMedia) {
-		// 			//	cam.src = stream;
-		// 			//} else {
-		// 				cam.src = window.URL.createObjectURL(stream) || stream;
-		// 			//}
-		// 			cam.play();
-		// 			// intervalId = setInterval(app.loop, loopSpeed);
-		// 			btnStart.style.display = "none";
-		// 			btnStop.style.display = "inline-block";
-		// 			app.loop()
-		// 		},
-		// 		function errorCallback(error) {
-		// 			window.alert("An error ocurred getting user media. Code:" + error.code);
-		// 		});
-		// } else {
-		// 	//Browser doesn't support user media
-		// 	window.alert("Your browser does not support user media");
-		// }
+		// If browser supports user media
+		if (navigator.getUserMedia) {
+			navigator.getUserMedia({video: true, toString: function () { return "video"; } },
+				function successCallback(stream) {
+					//if (navigator.getUserMedia === navigator.mozGetUserMedia) {
+					//	cam.src = stream;
+					//} else {
+						cam.src = window.URL.createObjectURL(stream) || stream;
+					//}
+					cam.play();
+					// intervalId = setInterval(app.loop, loopSpeed);
+					btnStart.style.display = "none";
+					btnStop.style.display = "inline-block";
+					app.loop()
+				},
+				function errorCallback(error) {
+					window.alert("An error ocurred getting user media. Code:" + error.code);
+				});
+		} else {
+			//Browser doesn't support user media
+			window.alert("Your browser does not support user media");
+		}
 
-		// e.preventDefault();
-  //   };
+		e.preventDefault();
+    };
 
-  //   app.stopCam = function (e) {
-		// clearInterval(intervalId);
-		// cam.src = "";
-		// e.preventDefault();
-		// btnStop.style.display = "none";
-		// btnStart.style.display = "inline-block";
-  //   };
+    app.stopCam = function (e) {
+		clearInterval(intervalId);
+		cam.src = "";
+		e.preventDefault();
+		btnStop.style.display = "none";
+		btnStart.style.display = "inline-block";
+    };
 
     app.findCharByColor = function(gray){
     	var index = (charArray.length-1)-Math.floor((gray/255)*charArray.length)
     	var char = charArray[index]
     	return char
     }
-    //The generation of the ascii text was taken from this great sample from thecodeplayer:
-    //http://thecodeplayer.com/walkthrough/cool-ascii-animation-using-an-image-sprite-canvas-and-javascript
     app.loop = function () {
 		var r, g, b, gray, pixels, colordata, character,
 		    line = "",
             i = 0;
 		
-		//clear canvas
 		canvasCtx.clearRect(0, 0, width, height);
 
-		//draw the video frame
 		canvasCtx.drawImage(cam, 0, 0, width, height);
 		
-		//accessing pixel data
 		pixels = canvasCtx.getImageData(0, 0, width, height);
 		colordata = pixels.data;
-
-		//every pixel gives 4 integers -> r, g, b, a
-		//so length of colordata array is width*height*4
 		
 		ascii.innerHTML = ''; //clear contents
 
@@ -95,38 +86,11 @@
 			b = colordata[i + 2];
 			//converting the pixel into grayscale
 			gray = r * 0.2126 + g * 0.7152 + b * 0.0722;
-			//overwriting the colordata array with grayscale values
-			//colordata[i] = colordata[i+1] = colordata[i+2] = gray;
 			character = app.findCharByColor(gray)
-			//text for ascii art.
-			//blackish = dense characters like "W", "@"
-			//whitish = light characters like "`", "."
-			// if (gray > 250) {
-   //              character = " "; //almost white
-   //          } else if (gray > 230) {
-   //              character = "`";
-   //          } else if (gray > 200) {
-   //              character = ":";
-   //          } else if (gray > 175) {
-   //              character = "*";
-   //          } else if (gray > 150) {
-   //              character = "+";
-   //          } else if (gray > 125) {
-   //              character = "#";
-   //          } else if (gray > 50) {
-   //              character = "W";
-   //          } else {
-   //              character = "@"; //almost black
-   //          }
-			
 
-
-			//newlines and injection into dom
 			if (i !== 0 && (i / 4) % width === 0) {//if the pointer reaches end of pixel-line
 				ascii.appendChild(document.createTextNode(line));
-				//newline
 				ascii.appendChild(document.createElement("br"));
-				//emptying line for the next row of pixels.
 				line = "";
 			}
 			
